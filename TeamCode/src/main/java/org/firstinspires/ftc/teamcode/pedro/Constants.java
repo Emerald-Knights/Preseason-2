eff6e7989bbbc85c30d9c5fb754eb7981cc03d31
Aarush <aarushgupta850@users.noreply.github.com>
2026-09-25 20:29:33 -0400

    Updated pedro 3.0.1 and sdk12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Constants.java
@@ -1,11 +1,47 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.revhub.drivetrains.MecanumConfig;
+import com.pedropathing.revhub.localizers.Encoder;
+import com.pedropathing.revhub.localizers.ThreeWheelConfig;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
+
+    // Copied from test branch (Pedro 2.x MecanumConstants)
+    public static MecanumConfig drivetrainConfig = new MecanumConfig(c -> {
+        c.frontLeftName.set("leftFront");
+        c.frontRightName.set("rightFront");
+        c.backLeftName.set("leftBack");
+        c.backRightName.set("rightBack");
+        c.frontLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+        c.frontRightDirection.set(DcMotorSimple.Direction.FORWARD);
+        c.backLeftDirection.set(DcMotorSimple.Direction.REVERSE);
+        c.backRightDirection.set(DcMotorSimple.Direction.FORWARD);
+    });
+
+    // Copied from test branch (Pedro 2.x ThreeWheelConstants)
+    public static ThreeWheelConfig localizerConfig = new ThreeWheelConfig(c -> {
+        c.leftEncoderName.set("leftBack");
+        c.rightEncoderName.set("rightBack");
+        c.strafeEncoderName.set("rightFront");
+        c.leftPodY.set(6.13);
+        c.rightPodY.set(-6.13);
+        c.strafePodX.set(-4.86);
+        c.forwardTicksToInches.set(0.001964593333);
+        c.strafeTicksToInches.set(-0.001973913907);
+        // 2.x called this turnTicksToInches. Re-run Three Wheel Tuner to confirm.
+        c.turnTicksToRadians.set(0.001912528435);
+        c.leftEncoderDirection.set(Encoder.FORWARD);
+        c.rightEncoderDirection.set(Encoder.REVERSE);
+        c.strafeEncoderDirection.set(Encoder.FORWARD);
+    });
+
+    // TODO: run Foresight Tuner, paste its foresightConfig here, then:
+    // return new Follower(new ThreeWheelLocalizer(h, localizerConfig), new Mecanum(h, drivetrainConfig), <Foresight algorithm>);
     public static Follower create(HardwareMap h) {
-        // return new Follower(Drivetrain, Localizer, Foresight);
         return null;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,5 +1,38 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.revhub.drivetrains.Mecanum;
+import com.pedropathing.revhub.localizers.ThreeWheelLocalizer;
+import com.pedropathing.tuning.autotune.Procedure;
+import com.pedropathing.tuning.autotune.Tuner;
+
+import org.firstinspires.ftc.teamcode.pedro.procedures.ForesightTuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.Tests;
+import org.firstinspires.ftc.teamcode.pedro.procedures.ThreeWheelTuner;
+
 public class Tuning {
-    // Tuners go here
+
+    @Tuner
+    public static Procedure mecanumTuner() {
+        return new MecanumTuner();
+    }
+    @Tuner
+    public static Procedure threeWheelTuner() {
+        return new ThreeWheelTuner();
+    }
+
+    @Tuner
+    public static Procedure foresightTuner() {
+        return new ForesightTuner(
+                h -> new ThreeWheelLocalizer(h, Constants.localizerConfig),
+                h -> new Mecanum(h, Constants.drivetrainConfig));
+    }
+
+    @Tuner
+    public static Procedure tests() {
+        return new Tests(
+                h -> new Mecanum(h, Constants.drivetrainConfig),
+                h -> new ThreeWheelLocalizer(h, Constants.localizerConfig),
+                null); // add Foresight algorithm after tuning
+    }
 }
